dfafbd0cd2bd8ce47a53c8e40c24cbe66ed7a620
Srinu <srinu18yt@gmail.com>
2026-09-21 10:37:21 +0530

    chore: remove scratch files from repo, extend gitignore

--- _pm_probe.py
+++
@@ -1,8 +0,0 @@
-import io
-p = r'app\src\main\assets\games\planetmerge\index.html'
-lines = io.open(p, encoding='utf-8').read().split('\n')
-pat = "$('#"
-hits = [(i, l.strip()[:150]) for i, l in enumerate(lines, 1) if pat in l]
-print('hash-dollar hits:', len(hits))
-for i, l in hits:
-    print(i, l)
